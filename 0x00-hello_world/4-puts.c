aab6a754e3e52c600158857efb35deadca43f3b9
amandamadlingozi <rjmadlingozi@gmail.com>
2023-02-17 10:46:37 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  main A coding language that prints a line with puts funtions
- *  Return: 0
+ * main - Entry
+ * Return: Always 0
  */
- int main(void) 
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
