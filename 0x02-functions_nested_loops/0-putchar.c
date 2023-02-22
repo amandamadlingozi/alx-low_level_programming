f367b40383a0188339ae188f6c31794fc9158243
amandamadlingozi <rjmadlingozi@gmail.com>
2023-02-22 12:43:52 -0800

    first Commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Program prints putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+		{
+		putchar(p[i]);
+		}
+		putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
