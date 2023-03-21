e122ebf4a73bae0e048cd95f3c39f740426eb4e2
amandamadlingozi <rjmadlingozi@gmail.com>
2023-03-21 11:49:42 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,40 +12,41 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i = 0, j = 0, k;
-	dog_t *dog;
-	
-	while (name[i] != '\0')
-		i++;
-	while (owner[j] != '\0')
-		j++;
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	dog->name = malloc(i *sizeof(dog->name));
-	if (dog->name == NULL)
-	{
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
-	for (k = 0; k <= i; k++)
-	{
-		dog->name[k] = name[k];
-		dog->age = age;
-		dog->owner = malloc(j *sizeof(dog->owner));
-	}
-	if (dog->owner == NULL)
-	{
-		free(dog->owner);
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
-	for (k = 0; k <= j; k++)
-		dog->owner[k] = owner[k];
-	return (dog);
+
+int i = 0, j = 0, k;
+dog_t *dog;
+
+while (name[i] != '\0')
+i++;
+while (owner[j] != '\0')
+j++;
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+{
+free(dog);
+return (NULL);
+}
+dog->name = malloc(i *sizeof(dog->name));
+if (dog->name == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+for (k = 0; k <= i; k++)
+{
+dog->name[k] = name[k];
+dog->age = age;
+dog->owner = malloc(j *sizeof(dog->owner));
+}
+if (dog->owner == NULL)
+{
+free(dog->owner);
+free(dog->name);
+free(dog);
+return (NULL);
+}
+for (k = 0; k <= j; k++)
+dog->owner[k] = owner[k];
+return (dog);
 }
