bc84fdbb9fcbbdd29282747c81b087a6810d8f90
amandamadlingozi <rjmadllingozi@gmail.com>
2023-11-02 11:57:46 -0700

    speak gently, she can hear

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,8 +6,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
+int append_text_to_file(const char *filenme, char *text_content);
 
-#endif /* MAIN_H */
+#endif 
